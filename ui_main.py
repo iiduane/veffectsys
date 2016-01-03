0335a19c4325c916d18352bc08beb75431fdc277
duane <iiduan@126.com>
2016-01-03 21:21:34 +0800

    duane: add file pattern search

--- ui_main.py
+++ ui_main.py
@@ -112,10 +112,41 @@ for subnode in subnodes:
 
 
 #### search keys  ####
+def find_file_by_pattern(pattern='.*', base="bbklog", circle=True):
+	'''查找给定文件夹下面所有 '''
+	re_file = re.compile(pattern)
+	if base == ".":
+		base = os.getcwd()
+
+	final_file_list = []
+	print base
+	cur_list = os.listdir(base)
+	for item in cur_list:
+		if item == ".svn" or item == ".repo" or item ==".git":
+			continue
+
+		full_path = os.path.join(base, item)
+		if full_path.endswith(".doc") or \
+			full_path.endswith(".bmp") or \
+			full_path.endswith(".wpt") or \
+			full_path.endswith(".dot"):
+			continue
+
+		# print full_path
+		bfile = os.path.isfile(item)
+		if os.path.isfile(full_path):
+			if re_file.search(full_path):
+				final_file_list.append(full_path)
+		else:
+			final_file_list += find_file_by_pattern(pattern, full_path)
+	return final_file_list
 
 
 ####  end search keys ####
 
+search_dir_url = "F:/CreativePrj/python_prj/veffectsys"
+file_list = find_file_by_pattern(".xml", search_dir_url)
+print file_list
 
 rootui = Tk()
 
@@ -226,6 +257,7 @@ frm_statusbar = Frame(frm_large, bg= 'grey')
 frm_statusbar.pack()
 statusbar_tw = Label(frm_statusbar, height=1, bg="pink",  width = large_xsize)
 statusbar_tw.pack(expand = YES, fill = X)
+
 
 
 frm_large.pack()
